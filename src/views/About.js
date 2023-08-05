dc3f897b8e31004446af4a7fce46e3cb9c322dc4
Filip <f@filip.world>
2023-08-04 22:11:56 -0400

    About view, further styling

--- src/views/About.js
+++ src/views/About.js
@@ -2,8 +2,32 @@ export function About() {
   return `
 <h1>About</h1>
 <p>
-  Bananapus is creating experimental tools to align communities across EVM
-  chains. Use at your own risk.
+  Bananapus was
+  <a href="https://www.jbdao.org/s/juicebox/368">funded by JuiceboxDAO</a> to
+  develop and implement solutions for cross-chain Juicebox-based organizations,
+  onchain governance based on staked positions, and new token distribution
+  strategies. This website and the
+  <a href="https://github.com/Bananapus/bananapus-721-staking-delegate"
+    >smart contracts it interacts with</a
+  >
+  are the first implementation of these objectives.
+</p>
+<p>
+  Bananapus will be deploying the Juicebox protocol on Ethereum layer 2
+  blockchains, and will deploy the first project on these versions of the
+  protocol and collect fees. Bananapus (and its projects) will be governed
+  onchain – on the <a href="#/stake">Stake</a> page, JBX holders can stake their
+  JBX to claim Bananapus governance NFTs. The more JBX someone stakes, and the
+  longer they stake it for, the more voting power they have over Bananapus.
+</p>
+<p>
+  Bananapus will reserve a percentage its project's tokens to be claimed by
+  stakers over a vesting period. Bananapus members can begin their vesting
+  periods and collect their rewards in the <a href="#/collect">Collect</a> page.
+</p>
+<p>
+  To learn more or to get involved, join our
+  <a href="https://discord.gg/ErQYmth4dS">Discord server</a>.
 </p>
 `;
 }
